2cf84b6e1021702ce440227ef0cee09c0908e19c
ctodmia <ctodmia@gmail.com>
2015-10-07 16:39:47 -0700

    function stacks, function queue, funcional shared stacks almost

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
+var stackMethods = {};
+var index = 0;
+var storage = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+
+  stackMethods.push = function (value) {
+    storage[index] = value;
+    index++;
+  }
+  stackMethods.pop = function () {
+    if (index>0){
+      var stored = storage[index-1];
+      delete storage[index-1];
+      index--;
+      return stored;
+    }
+  }
+
+  stackMethods.size = function () {
+    return index;
+  };
+  return stackMethods;
   // but try not not reference your old code in writing the new style.
+
+
 };
 
-var stackMethods = {};
-
+var NewMethod = {};
+NewMethod.push =   function(){
+  
+}
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function(){
   var someInstance = {};
+  index = 0;
+  remove =0; 
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,22 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[index + remove] = value;
+    index++;
   };
 
   someInstance.dequeue = function(){
+    if (index>0){
+      var stored = storage[remove];
+      delete storage[remove];
+      remove++;
+      index--;
+      return stored;
+    }
   };
 
   someInstance.size = function(){
+    return index;
   };
 
   return someInstance;
