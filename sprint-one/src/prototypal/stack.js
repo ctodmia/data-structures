def57573697c93d8f18d6bc3abd137c80bc8edb5
ctodmia <ctodmia@gmail.com>
2015-10-08 20:06:40 -0700

    prototypal stacks completed

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,51 +1,5 @@
 var Stack = function() {
- // Hey! Rewrite in the new style. Your code will wind up looking very similar,
- // but try not not reference your old code in writing the new style.
-    
- //var obj = {};
- //storage.value = value;
-  
-  storage = Object.create(stackMethods);
-
- storage.index = 0;
- 
-
- /*storage.size = stackMethods.size; 
- storage.push = stackMethods.push; 
- storage.pop = stackMethods.pop;*/ 
-
- return storage;
-};
-
-
-var stackMethods = {};
-
-    stackMethods.push : function(value) {
-
-    storage[this.index] = value;
-    //console.log(this.value);
-   this.index++;
-    
-    },
-    stackMethods.pop : function() {
-    if(this.index>0) {
-    var temp = storage[this.index-1]
-   delete storage[this.index-1];
-   this.index--; 
-   return temp;
-        }
-    },
-    
-    stackMethods.size : function () {
-        return this.index;
-    }
-
-
-
-
-
-
-/*var Stack = function() {
+  var test;
   storage = Object.create(stackMethods);
   storage.index = 0;
   return storage;
@@ -73,7 +27,13 @@ var stackMethods = {}; //Stack.stackMethods{};
   stackMethods.size = function(){
     return this.index;
   }
-*/
+
+
+
+
+
+
+
 
 
 
