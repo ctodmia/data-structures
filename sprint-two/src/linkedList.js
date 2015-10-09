1a855c778d5ecafd3df15b0b2abe8e73b88d1815
ctodmia <ctodmia@gmail.com>
2015-10-09 12:31:20 -0700

    one awesome test passed!!!! linked list.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,34 @@
 var LinkedList = function(){
+
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value){
+    //if new nodes are added to list, then replace tail with new nodes. 
+    // node.next === list.tail;
+    //at list.tail, node.next should = list.tail
+    if(list.head === null){
+      list.head=Node();
+      list.tail=list.head;
+    }else {
+      //list.tail.next=Node();
+      list.tail=list.head;
+    }
+    list.tail.value=value;
+  
+    
   };
 
-  list.removeHead = function(){
+  list.removeHead = function(value){
+    //should remove the head from the list when the removeHead is called.
+    // node.next = node.value;
+    //list.head.next = list.head.value;
+    //list.removeHead();
+    if (list.head.value){
+      list.head=Node();
+      list.head.value = list.head.value;
+    }
   };
 
   list.contains = function(target){
@@ -17,9 +39,10 @@ var LinkedList = function(){
 
 var Node = function(value){
   var node = {};
-
   node.value = value;
+  //value is the data held in the node 
   node.next = null;
+  //next is a referece to teh next node in the list
 
   return node;
 };
