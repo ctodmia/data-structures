30f854ed10a5258ab71b001ab5ad77dd454867b1
ctodmia <ctodmia@gmail.com>
2015-10-09 22:10:12 -0700

    set completed.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,44 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
+
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (typeof item === 'string') {
+    // console.log('this', item);
+    this._storage.push(item);
+    console.log('this.storage', this._storage);
+    
+    // console.log('store', this.set, "this", this );
+    // return more;
+  }
 };
 
 setPrototype.contains = function(item){
+  //console.log("item", item);
+  for (var i = 0; i < this._storage.length; i++) {
+    if(this._storage[i] === item){
+      return true;
+    }
+
+    //console.log("value", value, "this", this, "this.set", this.set);
+    
+    //console.log('contained', this.set, 'myitems', item);
+  }
+  return false;
+ 
 };
 
+  
 setPrototype.remove = function(item){
+   for(var i = 0; i < this._storage.length; i++){
+      if(this._storage[i] === item)
+        this._storage.splice(i, 1);
+   }
 };
 
 /*
