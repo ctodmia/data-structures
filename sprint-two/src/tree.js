b5b53c22bcdc2348ffddef1ab062bc87cbcbb649
ctodmia <ctodmia@gmail.com>
2015-10-10 16:43:41 -0700

    tree committed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,43 @@
 var Tree = function(value){
-  var newTree = {};
+ var newTree = {};
   newTree.value = value;
+ // newTree.addChild = treeMethods.addChild;
+ // newTree.contains = treeMethods.contains;
+ _.extend(newTree, treeMethods);
+ // your code here
+ newTree.children = [];  // fix me
 
-  // your code here
-  newTree.children = null;  // fix me
-
-  return newTree;
+ return newTree;
 };
-
-
 
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+    this.value = value // Update the value 
+    this.children.push(Tree(value));
 };
-
+ 
 treeMethods.contains = function(target){
+    for(var i = 0; i < this.children.length; i++){
+      for(var key in this.children[i]){
+        if(this.children[i][key]=== target){
+      return true;
+        } 
 
+      }
+    }
+
+    return false;
 };
 
+
+
+
+       
+    
 
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+* Complexity: What is the time complexity of the above functions?
+*/
